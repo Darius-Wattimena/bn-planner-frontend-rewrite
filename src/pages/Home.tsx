8d2dda20a356e233002e846e921cfaa04ad92c97
Darius Wattimena <dariuswattimena@hotmail.com>
2021-08-02 21:36:36 +0200

    Added simple grid and a barebones home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className={"container"}>
+      <h1>Nomination Planner</h1>
+      <p>By <a href="https://osu.ppy.sh/users/2369776">Greaper</a>, for osu! beatmap nominators</p>
+    </div>
+  )
+}
+
+export default Home
