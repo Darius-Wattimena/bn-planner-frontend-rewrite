fb09d86d087c3aa615867403ab7c934f26d11605
Darius Wattimena <dariuswattimena@hotmail.com>
2021-08-26 19:27:00 +0200

    Added status filters

---
+++ src/component/beatmaps/beatmapFilter/BeatmapStatusFilter.tsx
@@ -0,0 +1,100 @@
+import React, {useEffect, useState} from "react";
+import {BeatmapFilter, BeatmapStatus, SelectFilterItem} from "../../../models/Types";
+import {instantFilter} from "../../../utils/FilterUtils";
+import {getBeatmapStatus} from "../../../utils/BeatmapUtils";
+import Collapsible from "react-collapsible";
+
+interface BeatmapStatusFilterProps {
+  statuses: BeatmapStatus[]
+  beatmapFilter: BeatmapFilter
+  setBeatmapFormFilter: React.Dispatch<React.SetStateAction<BeatmapFilter>>
+  timeout: number
+  setQueryFilter: React.Dispatch<React.SetStateAction<BeatmapFilter>>
+}
+
+function BeatmapStatusFilter({ statuses, beatmapFilter, setBeatmapFormFilter, timeout, setQueryFilter }: BeatmapStatusFilterProps) {
+  const [selectedStatuses, setSelectedStatuses] = useState<number[]>([])
+  const [filterItems, setFilterItems] = useState<SelectFilterItem[]>([])
+
+  useEffect(() => {
+    const preparedStatuses = statuses.map((status, index) => {
+      const selectedStatus = selectedStatuses.find(item => item === status.id)
+      const item: SelectFilterItem = {
+        index: index,
+        label: status.name,
+        value: status.id,
+        selected: selectedStatus != null || selectedStatus !== undefined
+      };
+
+      return item
+    })
+
+    setFilterItems(preparedStatuses)
+  }, [selectedStatuses])
+
+  function removeNumber(value: number, numbers: number[]){
+    numbers.forEach( (item, index) => {
+      if(item === value) numbers.splice(index,1);
+    });
+  }
+
+  function updateSelectedItems(value: number, checked: boolean) {
+    const selectedStatuses = beatmapFilter["status"]
+    const newStatuses: number[] = [];
+
+    selectedStatuses.forEach(val => newStatuses.push(val))
+
+    if (checked) {
+      newStatuses.push(value)
+    } else {
+      removeNumber(value, newStatuses)
+    }
+
+    setSelectedStatuses(newStatuses)
+
+    instantFilter(
+      beatmapFilter,
+      "status",
+      newStatuses,
+      setBeatmapFormFilter,
+      timeout,
+      setQueryFilter
+    )
+  }
+
+  return (
+    <div className={"beatmap-filter-nominators"}>
+      <Collapsible
+        trigger={"Status"}
+        open={true}
+        className={"collapsible-parent-group"}
+        openedClassName={"collapsible-parent-group"}
+      >
+        <div className={"beatmap-filter-nominators-groups"}>
+          {filterItems.map((selectItem) => {
+            let statusClass = getBeatmapStatus(selectItem.value)?.className
+
+            return (
+              <div className={`beatmap-filter-status`}>
+                <input
+                  type="checkbox"
+                  id={`${selectItem.index}-status`}
+                  checked={selectItem.selected}
+                  onChange={event => {
+                    updateSelectedItems(selectItem.value, event.target.checked)
+                  }}
+                />
+                <label className={statusClass} htmlFor={`${selectItem.index}-status`}>
+                  {selectItem.label}
+                </label>
+              </div>
+            )
+          })}
+        </div>
+      </Collapsible>
+
+    </div>
+  )
+}
+
+export default BeatmapStatusFilter
