4dd7afa3d08f97c8954bc311f28274fc6de46f2a
Darius Wattimena <dariuswattimena@hotmail.com>
2022-04-18 20:46:19 +0200

    Small styling fixes

--- src/Settings.ts
+++ src/Settings.ts
@@ -5,6 +5,13 @@ export const DEV_ENV: Settings = {
   osu_redirect: "http://localhost:3000/login"
 };
 
+export const PROD_ENV: Settings = {
+  is_dev: true, // shows an indication when its dev
+  api_url: "https://newbnplannerbackend.greaper.net/",
+  osu_id: 14229, // osu! OAuth client ID
+  osu_redirect: "https://newbnplanner.greaper.net/login"
+};
+
 interface Settings {
   is_dev: boolean,
   api_url: string,
@@ -12,4 +19,4 @@ interface Settings {
   osu_redirect: string
 }
 
-export const CONFIG = DEV_ENV;
+export const CONFIG = PROD_ENV;
