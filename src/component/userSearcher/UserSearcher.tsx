c78477b8981dc7569657ddff623cc67d969d9b51
Darius Wattimena <dariuswattimena@hotmail.com>
2023-07-06 20:23:37 +0200

    Fixed an issue where the UserSearcher would not load in correctly as expected

--- src/component/userSearcher/UserSearcher.tsx
+++ src/component/userSearcher/UserSearcher.tsx
@@ -19,6 +19,7 @@ import Api from "../../resources/Api";
 import {cloneDeep} from "lodash";
 import {AxiosPromise, AxiosRequestConfig} from "axios";
 import {RefetchOptions} from "axios-hooks";
+import UserSearcherHotbar from "./UserSearcherHotbar";
 
 const filterDefaultState: UserSearchFilter = {
   username: null,
@@ -54,38 +55,28 @@ function UserSearcher(
     setBeatmapQueryFilter,
     execute
   }: UserSearcherProps) {
-  const defaultFilter = setupQueryFilterWithBeatmap()
-  const [userSearchFilter, setUserSearchFilter] = useState<UserSearchFilter>(defaultFilter)
-  const [queryFilter, setQueryFilter] = useState<UserSearchFilter>(defaultFilter)
+  const [userSearchFilter, setUserSearchFilter] = useState<UserSearchFilter>(filterDefaultState)
+  const [queryFilter, setQueryFilter] = useState<UserSearchFilter>(filterDefaultState)
   const [timeout, setTimeout] = useState<number>(0)
-  const [filterGamemodes, setFilterGamemodes] = useState<SelectFilterItem[]>([])
-  const [filterRoles, setFilterRoles] = useState<SelectFilterItem[]>([])
-  const [selectedGamemodes, setSelectedGamemodes] = useState<Gamemode[]>(userSearchFilter.gamemodes)
-  const [selectedRoles, setSelectedRoles] = useState<UserRole[]>(userSearchFilter.roles)
 
   function setupQueryFilterWithBeatmap() {
     let filter = cloneDeep(filterDefaultState)
     if (changingGamemode) {
       filter.gamemodes = [changingGamemode]
     } else if (beatmapFilter) {
-      filter.gamemodes = beatmapFilter?.gamemodes
+      filter.gamemodes = beatmapFilter.gamemodes
     }
-
-    console.log({filter})
 
     return filter
   }
 
   useEffect(() => {
     if (openUserSearcher) {
-      onOpen()
+      const startingQueryFilter = setupQueryFilterWithBeatmap()
+      setUserSearchFilter(startingQueryFilter)
+      setQueryFilter(startingQueryFilter)
     }
-
-  }, [openUserSearcher])
-
-  useEffect(() => {
-    onUpdateFilters()
-  }, [userSearchFilter, selectedGamemodes, selectedRoles])
+  }, [openUserSearcher, changingGamemode])
 
   function onSelectNominator(replacingUserId: string | undefined, newNominatorId: string | undefined) {
     if (changingGamemode && beatmapId && replacingUserId && newNominatorId && execute) {
@@ -102,8 +93,6 @@ function UserSearcher(
         newSelectedNominators.push(newNominatorId)
       }
 
-      console.log({newSelectedNominators, selectedNominators, replacingUserId, beatmapId})
-
       instantFilter(
         beatmapFilter,
         "nominators",
@@ -114,63 +103,6 @@ function UserSearcher(
       )
       setOpenUserSearcher(false)
     }
-  }
-
-  function remove<T>(value: T, array: T[]) {
-    array.forEach((item, index) => {
-      if (item === value) {
-        array.splice(index, 1);
-        return
-      }
-    });
-  }
-
-  function updateSelectedGamemodes(value: Gamemode, checked: boolean) {
-    const selectedField = userSearchFilter["gamemodes"]
-    const newGamemodes: Gamemode[] = [];
-
-    selectedField?.forEach(val => newGamemodes.push(val))
-
-    if (checked) {
-      newGamemodes.push(value)
-    } else {
-      remove(value, newGamemodes)
-    }
-
-    setSelectedGamemodes(newGamemodes)
-
-    instantFilter(
-      userSearchFilter,
-      "gamemodes",
-      newGamemodes,
-      setUserSearchFilter,
-      timeout,
-      setQueryFilter
-    )
-  }
-
-  function updateSelectedRoles(value: UserRole, checked: boolean) {
-    const selectedField = userSearchFilter["roles"]
-    const newRoles: UserRole[] = [];
-
-    selectedField?.forEach(val => newRoles.push(val))
-
-    if (checked) {
-      newRoles.push(value)
-    } else {
-      remove(value, newRoles)
-    }
-
-    setSelectedRoles(newRoles)
-
-    instantFilter(
-      userSearchFilter,
-      "roles",
-      newRoles,
-      setUserSearchFilter,
-      timeout,
-      setQueryFilter
-    )
   }
 
   function updateUsernameFilter(newValue: string) {
@@ -185,89 +117,7 @@ function UserSearcher(
     )
   }
 
-  function onUpdateFilters() {
-    let osuGamemode = selectedGamemodes.find(item => item === Gamemode.Osu)
-    let taikoGamemode = selectedGamemodes.find(item => item === Gamemode.Taiko)
-    let catchGamemode = selectedGamemodes.find(item => item === Gamemode.Catch)
-    let maniaGamemode = selectedGamemodes.find(item => item === Gamemode.Mania)
-
-    setFilterGamemodes([
-      {
-        index: 0,
-        label: "Osu",
-        value: "osu",
-        selected: osuGamemode != null || osuGamemode !== undefined,
-        disabled: changingGamemode != null && changingGamemode !== Gamemode.Osu
-      },
-      {
-        index: 1,
-        label: "Taiko",
-        value: "taiko",
-        selected: taikoGamemode != null || taikoGamemode !== undefined,
-        disabled: changingGamemode != null && changingGamemode !== Gamemode.Taiko
-      },
-      {
-        index: 2,
-        label: "Catch",
-        value: "fruits",
-        selected: catchGamemode != null || catchGamemode !== undefined,
-        disabled: changingGamemode != null && changingGamemode !== Gamemode.Catch
-      },
-      {
-        index: 3,
-        label: "Mania",
-        value: "mania",
-        selected: maniaGamemode != null || maniaGamemode !== undefined,
-        disabled: changingGamemode != null && changingGamemode !== Gamemode.Mania
-      }
-    ])
-
-    let nominatorRole = selectedRoles.find(item => item === "Nominator")
-    let probationRole = selectedRoles.find(item => item === "Probation")
-    let natRole = selectedRoles.find(item => item === "NominationAssessment")
-
-    setFilterRoles([
-      {
-        index: 0,
-        label: USER_ROLES.Nominator.short,
-        value: "Nominator",
-        selected: nominatorRole != null || nominatorRole !== undefined
-      },
-      {
-        index: 1,
-        label: USER_ROLES.Probation.short,
-        value: "Probation",
-        selected: probationRole != null || probationRole !== undefined
-      },
-      {
-        index: 2,
-        label: USER_ROLES.NAT.short,
-        value: "NominationAssessment",
-        selected: natRole != null || natRole !== undefined
-      }
-    ])
-  }
-
-  function onOpen() {
-    if (changingGamemode !== undefined) {
-      setUserSearchFilter({
-        username: null,
-        gamemodes: [changingGamemode],
-        roles: []
-      })
-    } else if (beatmapFilter !== undefined) {
-      setUserSearchFilter({
-        username: null,
-        gamemodes: beatmapFilter.gamemodes,
-        roles: []
-      })
-    }
-
-    onUpdateFilters()
-  }
-
   function onClose() {
-    setUserSearchFilter(filterDefaultState)
     setOpenUserSearcher(false)
   }
 
@@ -282,47 +132,13 @@ function UserSearcher(
       shouldCloseOnOverlayClick
     >
       <div className={"user-searcher-content"}>
-        <div className={"user-searcher-hotbar"}>
-          <h4 className={"user-searcher-header"}>Gamemodes</h4>
-          {filterGamemodes.map((selectItem, index) => {
-            return (
-              <div key={index}
-                   className={`user-searcher-select-item ${selectItem.disabled ? "disabled" : ""} ${index !== 0 ? "user-searcher-select-item-not-first" : ""}`}>
-                <input
-                  type="checkbox"
-                  id={`${selectItem.index}-usersearcher-gamemode`}
-                  checked={selectItem.selected}
-                  disabled={selectItem.disabled}
-                  onChange={event => {
-                    updateSelectedGamemodes(selectItem.value, event.target.checked)
-                  }}
-                />
-                <label className={"todo"} htmlFor={`${selectItem.index}-usersearcher-gamemode`}>
-                  {selectItem.label}
-                </label>
-              </div>
-            )
-          })}
-          <h4 className={"user-searcher-header"}>Roles</h4>
-          {filterRoles.map((selectItem, index) => {
-            return (
-              <div key={index}
-                   className={`user-searcher-select-item ${index !== 0 ? "user-searcher-select-item-not-first" : ""}`}>
-                <input
-                  type="checkbox"
-                  id={`${selectItem.index}-roles`}
-                  checked={selectItem.selected}
-                  onChange={event => {
-                    updateSelectedRoles(selectItem.value, event.target.checked)
-                  }}
-                />
-                <label className={"todo"} htmlFor={`${selectItem.index}-roles`}>
-                  {selectItem.label}
-                </label>
-              </div>
-            )
-          })}
-        </div>
+        <UserSearcherHotbar
+          changingGamemode={changingGamemode}
+          userSearchFilter={userSearchFilter}
+          timeout={timeout}
+          setUserSearchFilter={setUserSearchFilter}
+          setQuerySearchFilter={setQueryFilter}
+        />
         <div className={"user-searcher"}>
           <div className={"user-searcher-header"}>
             <h3>User Searcher</h3>
---
+++ src/component/userSearcher/UserSearcherHotbar.tsx
@@ -0,0 +1,243 @@
+import {Gamemode, SelectFilterItem, UserRole, UserSearchFilter} from "../../models/Types";
+import React, {Dispatch, SetStateAction, useEffect, useState} from "react";
+import {USER_ROLES} from "../../Constants";
+import {instantFilter} from "../../utils/FilterUtils";
+
+interface UserSearcherHotbarProps {
+  changingGamemode: Gamemode | undefined
+  userSearchFilter: UserSearchFilter
+  timeout: number
+  setUserSearchFilter: Dispatch<SetStateAction<UserSearchFilter>>
+  setQuerySearchFilter: Dispatch<SetStateAction<UserSearchFilter>>
+}
+
+function UserSearcherHotbar(
+  {
+    changingGamemode,
+    userSearchFilter,
+    timeout,
+    setUserSearchFilter,
+    setQuerySearchFilter
+  }: UserSearcherHotbarProps) {
+  const [filterGamemodes, setFilterGamemodes] = useState<SelectFilterItem[]>([])
+  const [filterRoles, setFilterRoles] = useState<SelectFilterItem[]>([])
+
+  useEffect(() => {
+    if (changingGamemode) {
+      let initialFilter = getInitialGamemodeFilter(changingGamemode)
+      setFilterGamemodes(initialFilter)
+    }
+
+  }, [changingGamemode])
+
+  useEffect(() => {
+    let changedFilterGamemodes = getUpdatedGamemodeFilter(userSearchFilter["gamemodes"])
+
+    if (changedFilterGamemodes !== filterGamemodes) {
+      setFilterGamemodes(changedFilterGamemodes)
+    }
+
+    let changedFilterRoles = getUpdatedRoleFilter(userSearchFilter["roles"])
+
+    if (changedFilterRoles !== filterRoles) {
+      setFilterRoles(changedFilterRoles)
+    }
+  }, [userSearchFilter])
+
+  function updateSelectedGamemodes(value: Gamemode, checked: boolean) {
+    const selectedField = userSearchFilter["gamemodes"]
+    const newGamemodes: Gamemode[] = [];
+
+    selectedField?.forEach(val => newGamemodes.push(val))
+
+    if (checked) {
+      newGamemodes.push(value)
+    } else {
+      remove(value, newGamemodes)
+    }
+
+    instantFilter(
+      userSearchFilter,
+      "gamemodes",
+      newGamemodes,
+      setUserSearchFilter,
+      timeout,
+      setQuerySearchFilter
+    )
+  }
+
+  function updateSelectedRoles(value: UserRole, checked: boolean) {
+    const selectedField = userSearchFilter["roles"]
+    const newRoles: UserRole[] = [];
+
+    selectedField?.forEach(val => newRoles.push(val))
+
+    if (checked) {
+      newRoles.push(value)
+    } else {
+      remove(value, newRoles)
+    }
+
+    instantFilter(
+      userSearchFilter,
+      "roles",
+      newRoles,
+      setUserSearchFilter,
+      timeout,
+      setQuerySearchFilter
+    )
+  }
+
+  function getUpdatedRoleFilter(newRoles: UserRole[]): SelectFilterItem[] {
+    let nominatorRole = newRoles.find(item => item === "Nominator")
+    let probationRole = newRoles.find(item => item === "Probation")
+    let natRole = newRoles.find(item => item === "NominationAssessment")
+
+    return [
+      {
+        index: 0,
+        label: USER_ROLES.Nominator.short,
+        value: "Nominator",
+        selected: nominatorRole != null || nominatorRole !== undefined
+      },
+      {
+        index: 1,
+        label: USER_ROLES.Probation.short,
+        value: "Probation",
+        selected: probationRole != null || probationRole !== undefined
+      },
+      {
+        index: 2,
+        label: USER_ROLES.NAT.short,
+        value: "NominationAssessment",
+        selected: natRole != null || natRole !== undefined
+      }
+    ]
+  }
+
+  function getUpdatedGamemodeFilter(newGamemodes: Gamemode[]): SelectFilterItem[] {
+    let osuGamemode = newGamemodes.find(item => item === Gamemode.Osu)
+    let taikoGamemode = newGamemodes.find(item => item === Gamemode.Taiko)
+    let catchGamemode = newGamemodes.find(item => item === Gamemode.Catch)
+    let maniaGamemode = newGamemodes.find(item => item === Gamemode.Mania)
+
+    return [
+      {
+        index: 0,
+        label: "Osu",
+        value: "osu",
+        selected: osuGamemode !== undefined,
+        disabled: changingGamemode && changingGamemode !== Gamemode.Osu
+      },
+      {
+        index: 1,
+        label: "Taiko",
+        value: "taiko",
+        selected: taikoGamemode !== undefined,
+        disabled: changingGamemode && changingGamemode !== Gamemode.Taiko
+      },
+      {
+        index: 2,
+        label: "Catch",
+        value: "fruits",
+        selected: catchGamemode !== undefined,
+        disabled: changingGamemode && changingGamemode !== Gamemode.Catch
+      },
+      {
+        index: 3,
+        label: "Mania",
+        value: "mania",
+        selected: maniaGamemode !== undefined,
+        disabled: changingGamemode && changingGamemode !== Gamemode.Mania
+      }
+    ]
+  }
+
+  function getInitialGamemodeFilter(gamemode: Gamemode | undefined): SelectFilterItem[] {
+    return [
+      {
+        index: 0,
+        label: "Osu",
+        value: "osu",
+        selected: gamemode === Gamemode.Osu,
+        disabled: gamemode && gamemode !== Gamemode.Osu
+      },
+      {
+        index: 1,
+        label: "Taiko",
+        value: "taiko",
+        selected: gamemode === Gamemode.Taiko,
+        disabled: gamemode && gamemode !== Gamemode.Taiko
+      },
+      {
+        index: 2,
+        label: "Catch",
+        value: "fruits",
+        selected: gamemode === Gamemode.Catch,
+        disabled: gamemode && gamemode !== Gamemode.Catch
+      },
+      {
+        index: 3,
+        label: "Mania",
+        value: "mania",
+        selected: gamemode === Gamemode.Mania,
+        disabled: gamemode && gamemode !== Gamemode.Mania
+      }
+    ]
+  }
+
+  function remove<T>(value: T, array: T[]) {
+    array.forEach((item, index) => {
+      if (item === value) {
+        array.splice(index, 1);
+        return
+      }
+    });
+  }
+
+  return (
+    <div className={"user-searcher-hotbar"}>
+      <h4 className={"user-searcher-header"}>Gamemodes</h4>
+      {filterGamemodes.map((selectItem, index) => {
+        return (
+          <div key={index}
+               className={`user-searcher-select-item ${selectItem.disabled ? "disabled" : ""} ${index !== 0 ? "user-searcher-select-item-not-first" : ""}`}>
+            <input
+              type="checkbox"
+              id={`${selectItem.index}-usersearcher-gamemode`}
+              checked={selectItem.selected}
+              disabled={selectItem.disabled}
+              onChange={event => {
+                updateSelectedGamemodes(selectItem.value, event.target.checked)
+              }}
+            />
+            <label className={"todo"} htmlFor={`${selectItem.index}-usersearcher-gamemode`}>
+              {selectItem.label}
+            </label>
+          </div>
+        )
+      })}
+      <h4 className={"user-searcher-header"}>Roles</h4>
+      {filterRoles.map((selectItem, index) => {
+        return (
+          <div key={index}
+               className={`user-searcher-select-item ${index !== 0 ? "user-searcher-select-item-not-first" : ""}`}>
+            <input
+              type="checkbox"
+              id={`${selectItem.index}-roles`}
+              checked={selectItem.selected}
+              onChange={event => {
+                updateSelectedRoles(selectItem.value, event.target.checked)
+              }}
+            />
+            <label className={"todo"} htmlFor={`${selectItem.index}-roles`}>
+              {selectItem.label}
+            </label>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default UserSearcherHotbar
